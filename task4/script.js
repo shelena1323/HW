d3a669b83a067b8b52766eea38e3f3c923cbe613
shelena1323 <shelena1323@gmail.com>
2022-12-01 15:18:46 +0400

    end

--- task4/script.js
+++ task4/script.js
@@ -1,12 +1,12 @@
 document.querySelector ('#ref').addEventListener('click', (event) =>
 {
     event.preventDefault();
-    console.log('Вы изменили название ссылки')
 })
 
 const ref = document.querySelector ('#ref').addEventListener('click', 
 function() {
     this.textContent = prompt('Измените текст ссылки');
+    console.log('Вы изменили название ссылки')
 })
 
 
--- task5/script.js
+++ task5/script.js
@@ -1,6 +1,11 @@
 let button = document.querySelector ('button');
 let input = document.querySelector('input');
 
+document.querySelector ('button').addEventListener('click', (event) =>
+{
+event.preventDefault();
+})
+
 button.onclick = function () {
 let A = document.querySelector ('#input').value;
 console.log(A);
